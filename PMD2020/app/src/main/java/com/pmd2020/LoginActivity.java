49639fdb962d50cb76839d919424271288448041
Emanuel Barrera <emanuelbarrera98@gmail.com>
2020-05-07 22:00:16 -0500

    Project

---
+++ PMD2020/app/src/main/java/com/pmd2020/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.pmd2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button login_btn;
+    private TextInputEditText user_editText;
+    private TextInputEditText password_editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        user_editText =(TextInputEditText)findViewById(R.id.user);
+        password_editText =(TextInputEditText)findViewById(R.id.password);
+
+        login_btn=(Button)findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(logIn()){
+                    String usr= user_editText.getText().toString();
+                    Intent intent= new Intent(LoginActivity.this,null);
+                    intent.putExtra("user",usr);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private boolean logIn() {
+        boolean login=false;
+        String usr= user_editText.getText().toString();
+        String pass= password_editText.getText().toString();
+        if(usr.equals("user") && (pass.equals("") || pass.equals("0000"))){
+            login=true;
+        }
+        return login;
+    }
+}
